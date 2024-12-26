86416895045e608178c058bf0c0b4a2870c5ee94
Sergio Quesada <Sergio Quesada>
2024-12-26 13:38:02 -0600

    Proyecto2

--- src/main/java/com/example/api_auth_users/services/AuthService/IAuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.api_auth_users.services.AuthService;
-
-import com.example.api_auth_users.DTO.TokenResponse;
-import com.example.api_auth_users.common.models.UserModel;
-
-public interface IAuthService {
-    TokenResponse createUser(UserModel userRequest);
-    UserModel mapToEntity(UserModel userRequest);
-}
--- src/main/java/com/example/api_auth_users/services/UserService/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.api_auth_users.services.UserService;
-
-import com.example.api_auth_users.common.models.UserModel;
-
-public interface IUserService {
-    UserModel getUser(Long userId);
-    void updateUser(UserModel userRequest, Long userId);
-    void deleteUser(Long userId);
-}
